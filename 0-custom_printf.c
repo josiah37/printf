570299fb554ee2c4b262c5f9458f6545d726d541
= <=>
2023-11-11 22:57:19 +0000

    new

--- 0-custom_printf.c
+++ 0-custom_printf.c
@@ -3,82 +3,58 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
  * _printf - print formated string into the standard console (stdout)
- * @format: format string
- *
+ *@format: format string
  * Return: number of char printed or -1 incase of failure
  */
 
-
-int print_string(char *str);
-
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list arg;
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(arg, format);
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-
-			if (*format == 'c')
-			{
-				char ch = va_arg(arg, int);
-
-				putchar(ch);
-				count++;
-			}
-
-			else if (*format == 's')
-			{
-				char *str = va_arg(arg, char *);
-
-				count = print_string(str);
-			}
-
-			else if (*format == '%')
-			{
-				putchar('%');
-				count++;
-			}
-		}
-		else
-		{
-			putchar(*format);
-			count++;
-		}
-		format++;
-	}
-	va_end(arg);
-	return (count);
-}
-
-/**
- * print_string - print string into stdout
- * @str: string pointe
- *
- * Return: number of chat
- */
-
-int print_string(char *str)
-{
         int count = 0;
+        va_list arg;
 
-        while (*str != '\0')
+        if (format == NULL)
+                return (-1);
+        va_start(arg, format);
+
+        while (*format != '\0')
         {
-                putchar(*str);
-                str++;
-                count++;
-        }
+                if (*format == '%')
+                {
+                        format++;
 
+                        if (*format == 'c')
+                        {
+                                char ch = va_arg(arg, int);
+                        putchar(ch);
+                        count++;
+                        }
+                        else if (*format == 's')
+                        {
+                                char *str = va_arg(arg, char *);
+
+                                while (*str != '\0')
+                                {
+                                        putchar(*str);
+                                        str++;
+                                        count++;
+
+                                }
+                        }
+                        else if (*format == '%')
+                        {
+                                putchar('%');
+                                count++;
+                        }
+                }
+                else
+                {
+                        putchar(*format);
+                        count++;
+                }
+                format++;
+        }
+        va_end(arg);
         return (count);
 }
