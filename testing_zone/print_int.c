0ceaec72406cd447e73ac9e0f69abe2ccdfc836a
josiah37 <eyosi50@gmail.com>
2023-11-13 00:28:39 +0300

    betty commplent for %d func

---
+++ testing_zone/print_int.c
@@ -0,0 +1,22 @@
+#include <stdiio.h>
+#include "main.h"
+int print_int(va_list arg)
+{
+    int count = 0;
+    int num = va_arg(arg, int);
+    char buffer[50];
+
+	itoaf(buffer, "%d", num);
+
+    char *ptr = buffer;
+   
+	 while (*ptr != '\0')
+	 {
+		 putchar(*ptr);
+		 ptr++;
+		 count++;
+	 }
+
+	 return (count);
+}
+
