99d72b9239a8a439f5bb258bdb2c91c700bf4bbf
Daniel Berihun <Danielshitaye10@gmail.com>
2023-11-13 15:24:45 -0800

    test

--- 0-custom_printf.c
+++ 0-custom_printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 	va_list arg;
 
 	va_start(arg, format);
-	if (format == NULL)
+	if (format == NULL && (format[0] == '\0' && format[1] == '\0'))
 		return (-1);
 
 	while (format != NULL && *format != '\0')
