07e7b1239649b9ce352604fb0378f948a441c43d
josiah37 <eyosi50@gmail.com>
2023-11-15 22:07:12 +0300

    pushing additonal files

---
+++ testing_zone/print_modulo.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_modulo - print & into stdout
+ *
+ * Return: 1
+ */
+
+int print_modulo(va_list arg)
+{
+	int ch = va_arg(arg, int);
+
+	ch = 37;
+
+	_putchar(ch);
+
+	return (1);
+}
